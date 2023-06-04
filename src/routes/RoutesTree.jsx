f1fa1be7c6052d4ef38a73825b4d588e7de32585
Ayashor <ayasho730@gmail.com>
2023-06-04 16:16:24 +0300

    highscore page initial layout

--- src/routes/RoutesTree.jsx
+++ src/routes/RoutesTree.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePageOut from "../pages/HomePageOut";
 import Signup from "../pages/Signup";
+import HighScorePage from "../pages/HighScorePage";
 
 const RoutesTree = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePageOut />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/highscores" element={<HighScorePage/>}/>
       {/* Add pages here */}
     </Routes>
   );
