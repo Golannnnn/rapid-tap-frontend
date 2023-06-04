65e0e71db2282682783da3bf13c9cdd876cee834
Golannnnn <forburacc@protonmail.ch>
2023-06-04 15:14:55 +0300

    testing game

--- src/routes/RoutesTree.jsx
+++ src/routes/RoutesTree.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePageOut from "../pages/HomePageOut";
 import Signup from "../pages/Signup";
+import TapMode from "../test/TapMode";
 
 const RoutesTree = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePageOut />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/tapmode" element={<TapMode />} />
       {/* Add pages here */}
     </Routes>
   );
--- src/test/TapMode.jsx
+++ src/test/TapMode.jsx
@@ -1,52 +1,114 @@
 import { useState, useEffect } from "react";
-import { Center } from "@chakra-ui/react";
+import Circle from "./Circle";
 
 const Game = () => {
-  const [dimensions, setDimensions] = useState({ width: 100, height: 100 });
+  const [round, setRound] = useState(1);
+  const [timer, setTimer] = useState(0);
+  const [circleDimensions, setCircleDimensions] = useState({
+    outerRadius: 100,
+    innerRadius: 50,
+  });
+  const [timeLimit, setTimeLimit] = useState(10); // Initial time limit in seconds
+  const [isGameRunning, setIsGameRunning] = useState(false);
 
-  const handleKeyDown = (event) => {
-    if (dimensions.width === 400 && dimensions.height === 400) {
-      alert("You win!");
-      return;
-    }
-    if (event.code === "Space") {
-      setDimensions((prevDimensions) => ({
-        width: prevDimensions.width + 10,
-        height: prevDimensions.height + 10,
-      }));
+  useEffect(() => {
+    // Update circle dimensions and time limit based on the round number
+    const calculateDimensions = (round) => {
+      // Calculate dimensions using an algorithm of your choice
+      const outerRadius = 100 + round * 10;
+      const innerRadius = outerRadius / 2;
+      return { outerRadius, innerRadius };
+    };
+
+    const calculateTimeLimit = (round) => {
+      // Calculate time limit using an algorithm of your choice
+      return 10 + round * 2; // Increase time limit by 2 seconds per round
+    };
+
+    const dimensions = calculateDimensions(round);
+    const limit = calculateTimeLimit(round);
+
+    setCircleDimensions(dimensions);
+    setTimeLimit(limit);
+    setTimer(0); // Reset the timer
+  }, [round]);
+
+  const startGame = () => {
+    setRound(1);
+    setTimer(0);
+    setIsGameRunning(true);
+  };
+
+  const handleKeyPress = (event) => {
+    if (isGameRunning && event.keyCode === 32) {
+      // Expand the inner circle
+      setCircleDimensions((prevDimensions) => {
+        const { outerRadius, innerRadius } = prevDimensions;
+        const newInnerRadius = innerRadius + 5;
+        if (newInnerRadius >= outerRadius) {
+          // Inner circle filled the outer circle, round completed
+          setRound((prevRound) => prevRound + 1);
+          setIsGameRunning(false);
+        }
+        return { ...prevDimensions, innerRadius: newInnerRadius };
+      });
     }
   };
 
   useEffect(() => {
-    document.addEventListener("keyup", handleKeyDown);
+    document.addEventListener("keydown", handleKeyPress);
     return () => {
-      document.removeEventListener("keyup", handleKeyDown);
+      document.removeEventListener("keydown", handleKeyPress);
     };
-  }, [dimensions]);
+  }, [isGameRunning]);
+
+  useEffect(() => {
+    let interval;
+    if (isGameRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isGameRunning]);
+
+  useEffect(() => {
+    if (timer >= timeLimit) {
+      // Round failed, reset the game
+      setRound(1);
+      setTimer(0);
+      setIsGameRunning(false);
+    }
+  }, [timer, timeLimit]);
 
   return (
-    <Center mt={200}>
+    <div>
+      <div>Round: {round}</div>
+      <div>Timer: {timer}</div>
       <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          width: "400px",
-          height: "400px",
-          border: "1px solid black",
-          borderRadius: "50%",
+          position: "relative",
+          marginTop: 150,
         }}
       >
-        <div
-          style={{
-            width: dimensions.width,
-            height: dimensions.height,
-            backgroundColor: "red",
-            borderRadius: "50%",
-          }}
-        ></div>
+        <Circle
+          radius={circleDimensions.outerRadius}
+          backgroundColor="transparent"
+          borderColor="black"
+        />
+        <Circle
+          radius={circleDimensions.innerRadius}
+          backgroundColor="black"
+          borderColor="black"
+        />
       </div>
-    </Center>
+      {!isGameRunning && <button onClick={startGame}>Start Game</button>}
+    </div>
   );
 };
 
