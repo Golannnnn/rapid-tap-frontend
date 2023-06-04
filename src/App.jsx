393c31714a4da46c26dd3c5476b2b7ba9051bdcd
DiegoPilosof <dlpilosof@gmail.com>
2023-06-04 16:06:37 +0300

    login updtate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import HomePageOut from "./components/HomePageOut";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
       <HomePageOut />
       <Signup />
+      <Login />
     </>
   );
 }
