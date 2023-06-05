965742317dfdbfecfd1ffd70179bb0063764ab61
Golannnnn <forburacc@protonmail.ch>
2023-06-05 17:11:33 +0300

    changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Flex, Text, Button, Image } from "@chakra-ui/react";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../context/UserContext";
 import { NavLink } from "react-router-dom";
-import logo from "../Images/logo.png"
+import logo from "../Images/logo.png";
 
 const Navbar = () => {
   const { user, logOut } = useContext(UserContext);
@@ -14,11 +14,14 @@ const Navbar = () => {
       justify="space-between"
       px={5}
       py={1}
-      borderBottom="1px solid black"
+      // borderBottom="1px solid black"
+      // bg="rgba(255, 255, 255, 0.9)"
     >
-      <Flex align='center'>
-        <Image src={logo} alt='RapidTap' boxSize='75px'/>
-        <Text ml={2}>{user ? `Welcome ${user.nickname}` : "Welcome Guest"}</Text>
+      <Flex align="center">
+        <Image src={logo} alt="RapidTap" boxSize="75px" />
+        <Text ml={2}>
+          {user ? `Welcome ${user.nickname}` : "Welcome Guest"}
+        </Text>
       </Flex>
       {user ? (
         <Button onClick={logOut}>Logout</Button>
