675db8526f2c8f8c89504a79e96f4e34cf173da6
Ayashor <ayasho730@gmail.com>
2023-06-05 12:16:50 +0300

    logged in user page, and score page updates

--- src/pages/HighScorePage.jsx
+++ src/pages/HighScorePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Flex, Heading, Text, Button } from "@chakra-ui/react";
 import ScoreTableItems from '../components/ScoreTableItems';
+import scoreServices from "../services/scores.js";
 import { NavLink } from 'react-router-dom';
 
 const HighScorePage = () => {
+  const [ prevScore , setPrevScore ] = useState([])
+  const [ bestScore , setBestScore ] = useState([])
+
+  useEffect(() => {
+    const fetchScores = async () => {
+      try{
+        const prevScoreData = await scoreServices.getLastScore();
+        setPrevScore(prevScoreData)
+        const bestScoreData = await scoreServices.getBestScore();
+        setBestScore(bestScoreData)
+      }catch(error){
+        console.error("Error fetching scores:", error);
+      }
+    }
+    fetchScores()
+  } , [])
+  
 
   return (
     <Flex direction='column' align='center' justify='center'>
@@ -12,8 +30,8 @@ const HighScorePage = () => {
         <Text align='center'>Where do you rank among the best?</Text>
       </Flex>
       <Flex direction='column' mt={4} mb={6} border='8px dotted black' p={4}>
-        <Text align='center'>Previous Score: 30</Text>
-        <Text align='center'>Highest Score: 100</Text>
+        <Text align='center'>Previous Score: {prevScore}</Text>
+        <Text align='center'>Highest Score: {bestScore}</Text>
       </Flex>
       <Flex direction='column'>
         <ScoreTableItems/>
--- src/pages/HomePageOut.jsx
+++ src/pages/HomePageOut.jsx
@@ -1,9 +1,12 @@
 import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { useContext } from "react";
 import { TbArrowBadgeRight } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
 
 const HomePageOut = () => {
   const navigate = useNavigate();
+  const { user } = useContext(UserContext)
 
   return (
     <Flex justify="center" align="center" direction="column">
@@ -11,27 +14,30 @@ const HomePageOut = () => {
         <Heading my={5} fontSize="4xl" textAlign="center">
           RapidTap
         </Heading>
-        <Text fontSize="xs" align="center">
-          Tap as fast as you can to fill the circle before time runs out!
-        </Text>
-        <Text fontSize="xs" align="center">
-          Login to save your score or play without saving!
-        </Text>
+        {!user ? (
+          <>
+            <Text fontSize="xs" align="center">
+              Tap as fast as you can to fill the circle before time runs out!
+            </Text>
+            <Text fontSize="xs" align="center">
+              Login to save your score or play without saving!
+            </Text>
+          </>
+        ) : (
+          <Text>What are you waiting for, {user.nickname}? Start tapping!</Text>
+        )}
       </Flex>
-      <Flex justify="center" align="center" direction="column" mt="150px">
+      <Flex justify="center" align="center" direction="column" mt={user ? "50px" : "75px"}>
         <Flex position="relative">
-          <Button
-            m={3}
-            className="glow-on-hover"
-            w="300px"
+          <Button m={3} className="glow-on-hover" w="300px"
             onClick={() => {
               navigate("/");
-            }}
-          >
+            }}>
             Play!
           </Button>
           <TbArrowBadgeRight size="60px" className="arrow-badge" />
         </Flex>
+        {!user && ( 
         <Flex position="relative">
           <Button
             m={3}
@@ -45,28 +51,30 @@ const HomePageOut = () => {
           </Button>
           <TbArrowBadgeRight size="60px" className="arrow-badge" />
         </Flex>
+      )}
         <Flex position="relative">
-          <Button
-            m={3}
-            className="glow-on-hover"
-            w="300px"
+          <Button m={3} className="glow-on-hover" w="300px"
+            onClick={() => {
+              navigate("/instructions");
+            }}>
+            How to Play?
+          </Button>
+          <TbArrowBadgeRight size="60px" className="arrow-badge" />
+        </Flex>
+        <Flex position="relative">
+          <Button m={3} className="glow-on-hover" w="300px"
             onClick={() => {
               navigate("/highscores");
-            }}
-          >
+            }}>
             Highscores
           </Button>
           <TbArrowBadgeRight size="60px" className="arrow-badge" />
         </Flex>
         <Flex position="relative">
-          <Button
-            m={3}
-            className="glow-on-hover"
-            w="300px"
+          <Button m={3} className="glow-on-hover" w="300px"
             onClick={() => {
               navigate("/settings");
-            }}
-          >
+            }}>
             Settings
           </Button>
           <TbArrowBadgeRight size="60px" className="arrow-badge" />
